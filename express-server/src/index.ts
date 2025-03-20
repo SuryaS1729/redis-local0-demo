ac6ac9c3e763a4dc6d73063f1225ca6daed192d3
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-03-20 18:52:11 +0530

    added the express side pushing to redis cue, pulling from worker side not done

---
+++ express-server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+
+import { createClient } from "redis";
+
+
+const app = express()
+
+app.use(express.json());
+
+
+const client = createClient()
+client.connect()
+
+app.post("/submit", async (req,res)=>{
+    const {problemId, userId, code, language } = req.body
+    //after this we ideally first send it to a prisma database postgres , you can implement that logic here
+  try {
+    await  client.lPush("submissions",JSON.stringify({problemId, userId, code, language}))
+    res.json({
+        message:"submission recieved"
+    })
+  } catch (e) {
+    res.json({
+        message:"submission failed"
+    })
+  }
+})
+
+app.listen(3000)
